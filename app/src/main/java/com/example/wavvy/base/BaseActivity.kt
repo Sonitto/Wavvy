86637f4ec21cae321b43b442bc8ec9124a76baaa
qsl-0901 <3441024350@qq.com>
2026-07-14 21:52:10 +0800

    firstbase

---
+++ app/src/main/java/com/example/wavvy/base/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.example.wavvy.base
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wavvy.R
+
+class BaseActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
